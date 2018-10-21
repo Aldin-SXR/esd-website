c48e61dd126995e2470765f871d3ec0fdbb53169
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2018-10-21 10:52:31 +0200

    Added new website template and changed folder reference name in index.js

--- rest/index.js
+++ rest/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 5000;
 let app = express();
 
-app.get("/", (req, res) => {
-    res.status(200).send("God's blessing to this wonderful world.");
-})
+app.use(express.static("public"));
 
 app.listen(port, () => {
     console.log("Server listening on port: " + port);
