e455daafbc7aaf6153b862a6431e5a1ed5480de7
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2018-10-22 11:31:42 +0200

    Created an initial testing controller.

---
+++ public/controllers/newsController.js
@@ -0,0 +1,34 @@
+const newsController = ($scope) => {
+    $scope.news = [
+        {
+            image: "img/categories/1.jpg",
+            title: "ESD Kick-Off Workshop",
+            description: "Lorem ipsum dolor sit amet, consectetur."
+        },
+        {
+            image: "img/categories/2.jpg",
+            title: "PCB Design Workshop",
+            description: "Lorem ipsum dolor sit amet, consectetur."
+        },
+        {
+            image: "img/categories/3.jpg",
+            title: "Raspberry Pi & IoT",
+            description: "Lorem ipsum dolor sit amet, consectetur."
+        },
+        {
+            image: "img/categories/4.jpg",
+            title: "ESP8266 Workshop",
+            description: "Lorem ipsum dolor sit amet, consectetur."
+        },
+        {
+            image: "img/categories/5.jpg",
+            title: "Collaboration with ACM Student Chapter",
+            description: "Lorem ipsum dolor sit amet, consectetur."
+        },
+        {
+            image: "img/categories/6.jpg",
+            title: "Line Follower Competition",
+            description: "Lorem ipsum dolor sit amet, consectetur."
+        }
+    ]
+};
