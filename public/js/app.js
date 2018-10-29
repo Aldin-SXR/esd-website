c3665bbb7865904edf41d8160815df672d6e2835
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2018-10-29 10:10:44 +0100

    Fixed initial map issues.

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,10 @@ app.config(($routeProvider) => {
     $routeProvider.when("/", {
         templateUrl: "views/home.html"
     }).when("/contact", {
-        templateUrl: "views/contact.html"
+        templateUrl: "views/contact.html"    
     })
 })
+
+/* Controllers */
 app.controller("newsController", newsController);
+app.controller("mapController", mapController);
