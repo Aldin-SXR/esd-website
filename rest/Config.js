cf6b77047b6d470206fdc205b0e01472a1ddd8fa
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2018-11-11 10:56:37 +0100

    Moved constants to Config.js

---
+++ rest/Config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    PORT:  process.env.PORT || 5000,
+    MONGODB_URI: process.env.MONGODB_URI ||  "mongodb://aldin_sxr:tanzmitmir1488@ds151533.mlab.com:51533/heroku_g03574n0"
+}
