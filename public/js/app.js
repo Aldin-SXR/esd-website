3519e36bab28e3590c5b5ceb595f8111a3240488
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-01-20 20:18:16 +0100

    Started making lead changes.

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,15 @@
 const app = angular.module("esd-app", [ "ngRoute" ]);
 app.config(($routeProvider) => {
     $routeProvider.when("/", {
-        templateUrl: "views/home.html"
+        templateUrl: "views/home.html",
+        activeTab: "home"
     }).when("/contact", {
-        templateUrl: "views/contact.html"    
+        templateUrl: "views/contact.html",
+        activeTab: "contact"
     })
 })
 
 /* Controllers */
 app.controller("newsController", newsController);
 app.controller("mapController", mapController);
+app.controller("linksController", linksController);
--- rest/Config.js
+++ rest/Config.js
@@ -1,4 +1,5 @@
 module.exports = {
     PORT:  process.env.PORT || 5000,
-    MONGODB_URI: process.env.MONGODB_URI ||  "mongodb://aldin_sxr:tanzmitmir1488@ds151533.mlab.com:51533/heroku_g03574n0"
+    // MONGODB_URI: process.env.MONGODB_URI ||  "mongodb://aldin_sxr:tanzmitmir1488@ds151533.mlab.com:51533/heroku_g03574n0"
+    MONGODB_URI: "mongodb://localhost:27017/esd_website"
 }
