a344d13fe302e4fb39d328fef1e1f47452d922f9
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-02-20 16:03:40 +0100

    Testing AdShield.

--- rest/index.js
+++ rest/index.js
@@ -11,7 +11,7 @@ const Config = require("./Config");
 let db;
 let app = express();
 
-app.use(cors());
+app.use(cors({credentials: true, origin: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", (req, res, next) => {
